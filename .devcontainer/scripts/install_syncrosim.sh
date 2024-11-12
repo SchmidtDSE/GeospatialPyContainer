86c70d43d871ebff4ec49fef2a1e55f6e92e9452
lucialayr <lucia.sophie.layritz@posteo.de>
2024-11-12 23:32:05 +0000

    installing mono noninteractively works!

--- .devcontainer/scripts/install_syncrosim.sh
+++ .devcontainer/scripts/install_syncrosim.sh
@@ -1,10 +1,19 @@
+echo 'test'
+
+#Catch time zone questions to install noninteractively
+DEBIAN_FRONTEND=noninteractive
+apt-get update
+ln -fs /usr/share/zoneinfo/Etc/UTC /etc/localtime
+apt-get install -y tzdata
+dpkg-reconfigure --frontend noninteractive tzdata
+
 apt-get -y install mono-complete
 
-chmod +x /root/SyncroSim/SyncroSim.Console.exe
+#chmod +x /root/SyncroSim/SyncroSim.Console.exe
 
-mono /root/SyncroSim/SyncroSim.Console.exe 
+#mono /root/SyncroSim/SyncroSim.Console.exe 
 
-mono /root/SyncroSim/SyncroSim.Console.exe 
+#mono /root/SyncroSim/SyncroSim.Console.exe --list --help
 
 #Console reference
 #https://docs.syncrosim.com/reference/console.html
