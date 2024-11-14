3af96212ad3b3e25ff7c625b749fb7d9d07fe79f
lucialayr <lucia.sophie.layritz@posteo.de>
2024-11-14 20:47:30 +0000

    Creates an empty SyncroSim library in container, but how to connect to an existing library is not yet clear

--- simulation/SagebrushSS.py
+++ simulation/SagebrushSS.py
@@ -1,11 +1,13 @@
-import pandas as pd
-
 import pysyncrosim as ps
 
-my_session = ps.Session(location = "syncrosim_linux_3_0_9")
+my_session = ps.Session(location="syncrosim_linux_3_0_9")
 
 print(my_session.packages())
 
-SBSR = ps.library(name = "SagebrushSteppeRestoration",
-                        session = my_session,
-                        overwrite = True)
+SBSR = ps.library(name="SagebrushSteppeRestoration",
+                        session=my_session,
+                        packages=["helloworldTimePy", "stsim"])
+
+print(SBSR)
+print(SBSR.info)
+print(SBSR.datasheets())
