f0ce7c320d7ecf6c671de879c02542d45bed93d8
lucialayr <lucia.sophie.layritz@posteo.de>
2024-11-15 02:26:25 +0000

    plotting Syncrosim data in notebook works

---
+++ simulation/spatialtest.py
@@ -0,0 +1,4 @@
+import xarray as xr
+import rioxarray as rxr
+
+ds = rxr.open_rasterio('root/ext/SagebrushSteppeRestoration.ssim/SagebrushSteppeRestoration.ssim.data/SimpleSpatialScenario/stsim_OutputSpatialState/sc.it1.ts2024.tif')
